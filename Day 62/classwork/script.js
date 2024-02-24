956219898723eda09c36e0dfd841a416459e231f
giogagnidze <giogagnie@gmail.com>
2024-02-24 15:05:27 +0400

    1th commit

---
+++ Day 62/classwork/script.js
@@ -0,0 +1,85 @@
+// // //car object
+// // const car = {
+// //     name: "fiat",
+// //     model: 500,
+// //     weight: 850,
+// //     color: "white"
+// // }
+// // car.year = 2010   //daemateba year objetshi
+// // car.name = "BMW"
+// // console.log(car.name)
+// // console.log(person.name)
+// // console.log(person.lastname)
+
+
+// // //peson object
+// // const person = {
+// //     name: "gio",
+// //     lastname: "gagnidze",
+// //     age: 17
+// // }
+
+// // //classwork 1.
+// // const friend = {
+// //     name: "deti",
+// //     lastname: "chxikvadze",
+// //     age: 14,
+// //     bestColor: "green"
+// // }
+// // const me = {
+// //     name: "gio",
+// //     lastname: "gagnidze",
+// //     age: 17,
+// //     bestColor: "black"
+// // }
+// // me.color = "purple"
+// // me.height = 160
+
+
+
+// // მეთოდების დამატება ობიექტებში
+
+// const car = {
+//     name: "fiat",
+//     model: 500,
+//     weight: 850,
+//     start: function(){
+//         console.log("Starting")
+//     }
+// }
+// car.start = function(){
+//     console.log("hello world")
+// }
+
+// car.break = function(seconds){
+//     for (let i = seconds; i >= 0; i--) {
+//         console.log("Break in " + i + " second");
+//       }
+// }
+// car.break(5)
+
+
+
+// ///////////////////////////
+
+// const name = "gio";
+
+// console.log(name.length)
+
+
+
+
+// const car = {
+//     brand: "Mazda",
+//     model: "rx7",
+//     start: () => "starting in 5 second",
+//     break: (second) => {
+//         for(let i = second; 1 > 0; 1--){
+//             console.log("car will break in " + i)
+//         }
+//     } 
+// };
+// console.log(car.start())
+// car.start = () => "ola amigo"
+// car.break(10)
+// console.log(car.start)
